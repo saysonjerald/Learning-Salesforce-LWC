0a646ec627c4c268285fa8857bfcf52ab5138574
Jerald Sayson <saysonjerald@gmail.com>
2023-02-09 16:23:46 +0800

    Added: More LWC and Jest

--- Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -59,3 +59,4 @@ describe('c-unit-test', () => {
     });
   });
 });
+
--- Debugging and Testing/test-lwc/jest.config.js
+++ Debugging and Testing/test-lwc/jest.config.js
@@ -2,5 +2,10 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
+    moduleNameMapper: {
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+      },
+      '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
+      '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
