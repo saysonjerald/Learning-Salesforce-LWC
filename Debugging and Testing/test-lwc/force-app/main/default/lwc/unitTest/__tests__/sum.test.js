023e620ead98f416624dc3b5742332684eb17882
Jerald Sayson <saysonjerald@gmail.com>
2023-02-09 15:12:39 +0800

    Added: LWC and Testing

--- Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++ Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -4,4 +4,4 @@ describe('sum()', () => {
   it('should add 1 and 2 returning 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-});
+}); 
--- Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/unitTest.js
+++ Debugging and Testing/test-lwc/force-app/main/default/lwc/unitTest/unitTest.js
@@ -4,6 +4,7 @@ import { sum } from './sum';
 export default class UnitTest extends LightningElement {
   @api unitNumber = sum(2,3);
   handleChange(event) {
+    // eslint-disable-next-line @lwc/lwc/no-api-reassignments
     this.unitNumber = event.target.value;
   }
 }
